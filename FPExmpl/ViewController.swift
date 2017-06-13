01f5152180fc5e544cd824605bd977f4305b3d01
Jose Galindo <jose.galindo@ironbit.com.mx>
2017-06-13 18:07:08 -0500

    How to use Finger prints

--- FPExmpl/ViewController.swift
+++ FPExmpl/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
@@ -20,6 +21,58 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func accessToData(_ sender: Any) {
+        let authContext = LAContext()
+        var mError : NSError?
+        if authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &mError) {
+            authContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics,
+                                       localizedReason: "Solo los elegidos pueden pasar", reply: { [unowned self](success : Bool, error : Error?) in
+                                        if success {
+                                            DispatchQueue.main.async() {
+                                                self.performSegue(withIdentifier: "showData", sender: self)
+                                            }
+                                        } else {
+                                            let mError = error as! LAError
+                                            switch (mError.code) {
+                                            case .appCancel:
+                                                print("Auth was cancelled")
+                                                break;
+                                            case .authenticationFailed:
+                                                print("Auth failed")
+                                                break;
+                                            case .invalidContext:
+                                                print("Invalid context")
+                                                break;
+                                            case .passcodeNotSet:
+                                                print("Passcode not Set")
+                                                break;
+                                            case .systemCancel:
+                                                print("System cancelled")
+                                                break;
+                                            case .touchIDLockout:
+                                                print("Touch lockout")
+                                                break;
+                                            case .touchIDNotAvailable:
+                                                print("Touch not available")
+                                                break;
+                                            case .touchIDNotEnrolled:
+                                                print("Touch not enroled")
+                                                break;
+                                            case .userCancel:
+                                                print("user cancelled")
+                                                break;
+                                            case .userFallback:
+                                                print("user fallback")
+                                                break;
+                                            }
+                                        }
+                
+            })
+        } else {
+            print("No tiene lector de huella")
+        }
+        
+    }
 
 }
 
